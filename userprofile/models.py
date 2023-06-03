b5612d80c35a78cf53b2f52519798e1b0c610f5e
Jimmy Vaniski da Silva <Vaniskito@hotmail.com>
2023-06-03 06:32:14 +0100

    build: created userprofile app
    created model Userprofile and registred in admin

---
+++ userprofile/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
