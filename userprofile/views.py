8dd4687890237a2ba031bc76f36b319c3e9e3176
Jimmy Vaniski da Silva <Vaniskito@hotmail.com>
2023-06-07 18:54:40 +0100

    build: create product in frontend
    funcao para cadastrar produto pelo front end.

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
+from .models import UserProfile
+from shop.forms import ProductForm
+from shop.models import Product, Category
 
 
 def vendor_details(request, pk):
@@ -9,11 +14,33 @@ def vendor_details(request, pk):
 
     return render(request, 'userprofile/vendor_details.html', {'user': user})
 
-
+@login_required
 def my_shop(request):
     return render(request, 'userprofile/my_shop.html')
 
 
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = request.POST.get('title')
+            product = form.save(commit=False)
+            product.user = request.user
+            product.slug = slugify(title)
+            product.save()
+            return redirect('my_shop')
+    else:
+        form = ProductForm()
+    return render(request, 'userprofile/add_product.html', {'form': form})
+
+
+@login_required
+def edit_product(request):
+    return render(request, 'userprofile/my_shop.html')
+
+
+@login_required
 def my_account(request):
     return render(request, 'userprofile/my_account.html')
 
