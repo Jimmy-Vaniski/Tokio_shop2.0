f2db63f56b42697a96635d363e77837b2217bb08
Jimmy Vaniski da Silva <Vaniskito@hotmail.com>
2023-06-10 15:12:15 +0000

    build(crud): crud products
    terminado funcao para deletar produtos associando a sessao refatorando e implementando partials

--- shop/forms.py
+++ shop/forms.py
@@ -1,13 +1,50 @@
 from django import forms
 from .models import Product
+from django.utils.text import slugify
 
 
 class ProductForm(forms.ModelForm):
+    price = forms.DecimalField(
+        widget=forms.TextInput(attrs={
+            'class': 'w-full p-4 border border-green-600',
+            'placeholder': 'Digite o preço do produto',
+        })
+    )
+    stock = forms.IntegerField(
+        widget=forms.TextInput(attrs={
+            'class': 'w-full p-4 border border-green-600',
+            'placeholder': 'Digite a quantidade em estoque',
+        })
+    )
+
     class Meta:
         model = Product
-        fields = ['category', 'title', 'description', 'price', 'image']
+        fields = ('category', 'title', 'description', 'price', 'image', 'stock', 'shelf')
         widgets = {
-            'category': forms.Select(attrs={'class': 'p-4 border-radios border-green-300'}),
-
-
+            'category': forms.Select(attrs={
+                'class': 'w-full p-4 border border-green-600',
+                'placeholder': 'Selecione a categoria',
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'w-full p-4 border border-green-600',
+                'placeholder': 'Digite o título do produto',
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full p-4 border border-green-600',
+                'placeholder': 'Digite a descrição do produto',
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'w-full p-4 border border-green-600',
+            }),
+            'shelf': forms.TextInput(attrs={
+                'class': 'w-full p-4 border border-green-600',
+                'placeholder': 'Digite o local de armazenamento',
+            }),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        if title:
+            cleaned_data['slug'] = slugify(title)
+        return cleaned_data
